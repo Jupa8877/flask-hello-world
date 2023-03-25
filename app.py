a77865366675210bc70faf3c9d0e32cbcf2471bd
Jupa8877 <111999240+Jupa8877@users.noreply.github.com>
2023-03-25 13:26:46 -0600

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def selecting():
         response_string+="<tr>"
         for info in player:
             response_string+="<td>{}</td>".format(info)
-        response_string+="</table>"
+    response_string+="</table>"
     return response_string
 
 
