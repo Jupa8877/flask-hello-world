b00704f9c2499551a9d0bd5fc0319e47047fedb5
Jupa8877 <111999240+Jupa8877@users.noreply.github.com>
2023-03-25 13:16:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -44,8 +44,8 @@ def inserting():
         INSERT INTO Basketball (First, Last, City, Name, Number)
         VALUES
         ('Jayson', 'Tatum', 'Boston', 'Celtics', 0), 
-        ('Stephen', 'Curry', San Francisco', 'Warriors', 30),
-        ('Nikola', Jokic', 'Denver', Nuggets', 15),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
         ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
     ''')
     conn.commit()
